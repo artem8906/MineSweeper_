016a6ff0f4083bd883014e430b1377b8e6c5fe79
Artem Peshkov <a.peshkov@fstan.ru>
2022-07-12 21:20:29 +0200

    Second task was done

---
+++ src/minesweeper/Minesweeper.java
@@ -0,0 +1,32 @@
+package minesweeper;
+
+import minesweeper.consoleui.ConsoleUI;
+import minesweeper.core.Field;
+
+/**
+ * Main application class.
+ */
+public class Minesweeper {
+    /** User interface. */
+    private ConsoleUI userInterface;
+ 
+    /**
+     * Constructor.
+     */
+    private Minesweeper() {
+        userInterface = new ConsoleUI();
+        
+        Field field = new Field(9, 9, 10);
+        userInterface.newGameStarted(field);
+    }
+
+    /**
+     * Main method.
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("game has started");
+        new Minesweeper();
+
+    }
+}
